a2ecb669cd6a3920c160855ca33ef36d75d76133
Kim YJ <gallagherismine@gmail.com>
2022-07-29 22:32:28 +0900

    SpringBoot 기본 프로젝트 생성

---
+++ Springboot/src/main/java/com/moonnari1/restfulwebservice/MoonApiApplication.java
@@ -0,0 +1,13 @@
+package com.moonnari1.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonApiApplication.class, args);
+    }
+
+}
---
+++ Springboot/src/test/java/com/moonnari1/restfulwebservice/MoonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonnari1.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
