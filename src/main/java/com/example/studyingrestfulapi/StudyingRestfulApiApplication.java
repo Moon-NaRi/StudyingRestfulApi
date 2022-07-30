139113dd7cc3cabcd7ff9b24f276f45333ae04df
Kim YJ <gallagherismine@gmail.com>
2022-07-30 16:07:25 +0900

    다국적 언어처리 추가

--- src/main/java/com/example/studyingrestfulapi/StudyingRestfulApiApplication.java
+++ src/main/java/com/example/studyingrestfulapi/StudyingRestfulApiApplication.java
@@ -2,6 +2,11 @@ package com.example.studyingrestfulapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class StudyingRestfulApiApplication {
@@ -10,4 +15,12 @@ public class StudyingRestfulApiApplication {
         SpringApplication.run(StudyingRestfulApiApplication.class, args);
     }
 
+    //초기화시 메모리에 올라감
+    @Bean
+    public LocaleResolver localeResolver() {
+        //한국어 기본 처리 추가
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
+    }
 }
--- src/main/java/com/example/studyingrestfulapi/helloWorld/HelloWorldController.java
+++ src/main/java/com/example/studyingrestfulapi/helloWorld/HelloWorldController.java
@@ -1,8 +1,13 @@
 package com.example.studyingrestfulapi.helloWorld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 //xml이 아니라 어노테이션으로 컨트롤러를 지원함
 //view를 갖지 않는 Rest Data(json/xml)을 반환
@@ -10,6 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 //RestController는 Controller와 ResponseBody 역할을 모두 해서 클라이언트로 바로 반환
 @RestController
 public class HelloWorldController {
+
+    @Autowired  //같은 타입을 가진 Bean 자동 연결
+    private MessageSource messageSource;
+
     //Get방식
     // /hello-world endpoint
     @GetMapping(path = "/hello-world")
@@ -27,5 +36,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+        //설정 안하면 default korea
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }
